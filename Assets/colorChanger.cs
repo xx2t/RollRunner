bdb6f37caa660ef4f8656a3a43d1fdcc5143d9e5
Student <student@DM019.local>
2019-10-31 08:51:22 -0500

    InitialCommit

---
+++ Assets/colorChanger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorChanger : MonoBehaviour
+{
+    Renderer rend;
+    void Start(){
+        rend=this.GetComponent<Renderer>();
+    }
+    void Update(){
+        if(input.anyKeyDown){
+            rend.material.color=Random.ColorHSV();
+        }
+    }
+}
